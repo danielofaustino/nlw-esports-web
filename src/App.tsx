b44be4eedaa584842adfccc99e79a4d1adeb8e59
Daniel Faustino <danielofaustino@proton.me>
2022-09-12 23:02:31 -0300

    feat: game cards, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-interface ButtonProps {
-  color: string
-  texto: string
-  p:string
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button style={{ backgroundColor: props.color,padding:props.p }}>
-      {props.texto}
-    </button>
-  )
-}
+import './styles/main.css'
+import { MagnifyingGlassPlus } from 'phosphor-react'
+import LogoImg from './assets/logo-nlw-esports.svg'
 
 function App() {
 
+  let images = ['./game-1.png', './game-2.png', './game-3.png', './game-4.png', './game-5.png', './game-6.png']
+
   return (
-    <div>
-      <Button color='red' texto="Nanai" p='5%' />
-      <Button color="blue" texto="Daniel" p='2%' />
-      <Button color="pink" texto="Daniel" p='2%' />
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={LogoImg} alt="logo nlw sports" />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+
+
+        {images.map((image) => {
+          return (
+            <a href="" className='relative rounded-lg overflow-hidden'>
+              <img src={image} alt="" />
+              <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+                <strong className="font-bold text-white block">League of Legends</strong>
+                <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+              </div>
+            </a>
+          )
+        })}
+
+      </div>
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600  text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
